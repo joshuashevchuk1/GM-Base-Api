a6e9b1d7ca1faf67bc9c5175265008911e2d7cf0
joshuashevchuk <joshua.shevchuk@ookla.com>
2025-05-01 21:23:58 -0700

    update model components

--- src/database/model.py
+++ src/database/model.py
@@ -1,14 +1,17 @@
 from mongoengine import Document, StringField, FileField
 
 class MeetDocument(Document):
-    meet_key = StringField(required=True, unique=True)
+    meet_key = StringField(required=True, unique=True) # google meet key
 
-    space_name = StringField(required=False)
-    topic_name = StringField(required=False)
-    space_uri = StringField(required=False)
+    space_name = StringField(required=False) # name of the google meet space
+    space_uri = StringField(required=False)  # url of the google meet space
 
-    transcript = FileField()
-    recording = FileField()
+    topic_name = StringField(required=False) # name of the topic subscribed to this google meet
+
+    transcript = FileField() # the transcript file post google meet
+    recording = FileField() # the audio recording file post google meet
+
+    conversation_history = StringField(required=False)
 
     meta = {
         'collection': 'meet_spaces',
