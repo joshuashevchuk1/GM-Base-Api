bd2285468e0c1069b4659e7cbb55e09448af5d4b
joshuashevchuk <joshua.shevchuk@ookla.com>
2025-05-01 21:27:21 -0700

    added comments to the model

--- src/database/model.py
+++ src/database/model.py
@@ -1,15 +1,21 @@
 from mongoengine import Document, StringField, FileField
 
 class MeetDocument(Document):
-    meet_key = StringField(required=True, unique=True) # google meet key
+    # the google meet key for the space
+    meet_key = StringField(required=True, unique=True)
 
-    space_name = StringField(required=False) # name of the google meet space
-    space_uri = StringField(required=False)  # url of the google meet space
+    # name of the google meet space
+    space_name = StringField(required=False)
+    # url of the google meet space
+    space_uri = StringField(required=False)
 
+    # name of the topic subscribed to this google meet
     topic_name = StringField(required=False) # name of the topic subscribed to this google meet
 
-    transcript = FileField() # the transcript file post google meet
-    recording = FileField() # the audio recording file post google meet
+    # the transcript file post google meet
+    transcript = FileField()
+    # the audio recording file post google meet
+    recording = FileField()
 
     # all history dealt with ai chat for the post meeting session
     convo_history = StringField(required=False)
