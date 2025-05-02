5f4d6c58b2bef0696171b7f12cd23cc5af7dec70
joshuashevchuk <joshua.shevchuk@ookla.com>
2025-05-01 19:25:46 -0700

    update mongo properly

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -1,7 +1,6 @@
 # handlers/__init__.py
 
-from .home import router as home_router
-from .health_ import router as health_router
-from src.schemas.transcript import router as transcript_router
+from .home import router as home
+from .health_ import router as health
 
-routers = [home_router, health_router,transcript_router]
+handlers = [home, health]
