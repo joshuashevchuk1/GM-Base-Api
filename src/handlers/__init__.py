668cf489e470553c815e0604a2b9216ccbdd8db0
joshuashevchuk <joshua.shevchuk@ookla.com>
2025-05-01 22:40:54 -0700

    include new routes

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -5,11 +5,19 @@ from .health_ import router as health
 from .space_name import router as space
 from .meet_document import router as meet_document
 from .transcript import router as transcript
+from .recording import router as recording
+from .space_uri import router as space_uri
+from .topic_name import router as topic_name
+from .convo_history import router as convo_history
 
 handlers = [
     home,
     health,
     space,
+    space_uri,
     meet_document,
-    transcript
+    topic_name,
+    transcript,
+    recording,
+    convo_history
 ]
