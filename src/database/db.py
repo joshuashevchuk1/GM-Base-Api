c61d7ee899abe533b047fbfc2fb0cd2d53c2b335
joshuashevchuk <joshua.shevchuk@ookla.com>
2025-05-03 17:28:55 -0700

    update db

--- src/database/db.py
+++ src/database/db.py
@@ -1,13 +1,18 @@
+from src.config import config
 from mongoengine import connect
 
 
 class GoogleDb():
     def __init__(self):
         self.db = None
+        self.config = config.Config()
 
     def initialize_db(self):
+        #host = self.config.get_mongo_host()
+        host = "localhost"
+        print(host)
         connect(
-            host='localhost',
+            host=host,
             port=27017,
             authentication_source='admin',
             name='gm_meet',
