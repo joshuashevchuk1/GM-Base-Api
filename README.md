1cb8351e11447f6b198ab6c33f861b742e8a2044
joshuashevchuk <joshua.shevchuk@ookla.com>
2025-06-05 22:14:15 -0700

    fix docker file for gm-base-api

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # GM-Base-Api
 
+## Quick guide
+
+This api provides the allows access to db handling. 
+It can be built using the following docker commands
+
 ```commandline
 docker build -t gm-base-api .
 ```
@@ -7,6 +12,8 @@ docker build -t gm-base-api .
 ```commandline
 docker run -d -p 8080:8080 gm-base-api
 ```
+
+Running, on local host you can access the docs at /doc or /redoc
 
 contributors:
 -josh shevchuck
--- src/config/config.py
+++ src/config/config.py
@@ -1,86 +1,14 @@
-import os
-import yaml
-import logging
-import logging.config
-
-
 class Config:
-    _instance = None
-
-    def __new__(cls, env=None, path="../config"):
-        if cls._instance is None:
-            cls._instance = super(Config, cls).__new__(cls)
-            # Ensure instance gets initialized with env and path on first instantiation
-            cls._instance._init(env, path)
-        return cls._instance
-
-    def __init__(self, env=None, path="./config"):
-        # Make sure __init__ does not reinitialize if it has already been initialized
-        if not hasattr(self, 'initialized'):
-            self.env = env or os.getenv('STAGE', 'dev')
-            self.path = path
-            self.config = self.load_config()
-            self.logging = None
-            self.setup_logging()
-
-    def _init(self, env, path):
-        """
-        Private initialization method to initialize env and path
-        only when the singleton is first created.
-        """
-        self.env = env or os.getenv('STAGE', 'dev')
-        self.path = path
-
-    def load_config(self):
-        config_file = os.path.join(self.path, f'{self.env}.yaml')
-        # Load from local file system for 'local' environment using self.path
-        try:
-            with open(config_file, 'r') as file:
-                config = yaml.safe_load(file)
-            return config
-        except FileNotFoundError:
-            raise FileNotFoundError(f"Local configuration file {config_file} not found")
-        except Exception as e:
-            raise FileNotFoundError(f"Error loading local configuration file {config_file}: {str(e)}")
-
-    def setup_logging(self):
-        logging_config = self.get('logging')
-        if logging_config:
-            logging.config.dictConfig(logging_config)
-        else:
-            logging.basicConfig(level=logging.DEBUG)
-            logging.warning("No logging configuration found; using default settings.")
-
-        # Attach the root logger to the config
-        self.logging = logging.getLogger()
+    def __init__(self):
+        self.mongo_host = "localhost"
+        self.mongo_port = int(27017)
+        self.mongo_name = "gm_meet"
 
     def get_mongo_host(self):
-        return self.get('mongo.host')
+        return self.mongo_host
 
     def get_mongo_port(self):
-        return self.get('mongo.port')
+        return self.mongo_port
 
     def get_mongo_name(self):
-        return self.get('mongo.name')
-
-    def get_mongo_user(self):
-        return self.get('mongo.user')
-
-    def get_mongo_pass(self):
-        return self.get('mongo.pass')
-
-    def get_download_path(self):
-        return self.get("download_path")
-
-    def get_extract_dir(self):
-        return self.get('extract_dir')
-
-    def get(self, key, default=None):
-        keys = key.split('.')
-        value = self.config
-        try:
-            for k in keys:
-                value = value[k]
-        except KeyError:
-            return default
-        return value
+        return self.mongo_name
